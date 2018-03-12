89d73fc886b178b140dc764a90f45b3eed8f106d
Stephen March <sdmarch@utexas.edu>
2018-03-12 13:28:10 -0500

    Update organize_data.m

--- organize_data.m
+++ organize_data.m
@@ -1,3 +1,3 @@
-output_file = 'data_summary_2017-01-06';
-FTIR = 1;
+output_file = 'data_summary_2017-01-06'; % <output_file_name>_aggregate.txt
+FTIR = 1;  % uses FTIR regex
 data_aggregate(output_file,FTIR,pwd);

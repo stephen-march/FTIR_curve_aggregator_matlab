9b7620a2b0bce6754e3cc853440c592adf56405b
Stephen March <sdmarch@utexas.edu>
2018-03-12 13:31:40 -0500

    Update organize_data.m

--- organize_data.m
+++ organize_data.m
@@ -1,3 +1,4 @@
 output_file = 'data_summary_2017-01-06'; % <output_file_name>_aggregate.txt
 FTIR = 1;  % uses FTIR regex
-data_aggregate(output_file,FTIR,pwd);
+plot_on = 1; % generates a sample plot
+data_aggregate(output_file,FTIR,pwd,plot_on);
